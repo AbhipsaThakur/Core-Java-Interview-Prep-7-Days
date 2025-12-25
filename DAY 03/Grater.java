1672f0e6dc0b23dcea595512f9719157b19750b0
AbhipsaThakur <abhipsathaku933@gmail.com>
2025-12-25 09:14:18 +0530

    adding program 3

---
+++ DAY 03/Grater.java
@@ -0,0 +1,10 @@
+/*Write a program to find the grater number among 2 integers using ternary operator*/
+
+public class Grater {
+    public static void main(String[] args) {
+        int a = 7, b = 5;
+        String s = (a > b) ? "a is grater" : "b is grater";
+        System.out.println(s);
+    }
+}
+
